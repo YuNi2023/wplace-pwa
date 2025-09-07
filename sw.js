cb58be7a775482ce12fefb3ff06089a464e9dbe5
YuNi2023 <123335749+YuNi2023@users.noreply.github.com>
2025-09-07 15:03:48 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,23 @@
 self.addEventListener('push', (event) => {
-  const data = event.data?.json?.() ?? {};
-  event.waitUntil(self.registration.showNotification(
-    data.title || 'Wplace通知',
-    { body: data.body || '', icon: '/icons/icon-192.png', badge: '/icons/icon-192.png' }
-  ));
+  let data = {};
+  try {
+    data = event.data ? event.data.json() : {};
+  } catch (e) {
+    data = {};
+  }
+  event.waitUntil(
+    self.registration.showNotification(
+      data.title || 'Wplace通知',
+      {
+        body: data.body || '',
+        icon: './icons/icon-192.png',
+        badge: './icons/icon-192.png'
+      }
+    )
+  );
 });
 
 self.addEventListener('notificationclick', (event) => {
   event.notification.close();
-  event.waitUntil(clients.openWindow('/'));
+  event.waitUntil(clients.openWindow('./'));
 });
-
